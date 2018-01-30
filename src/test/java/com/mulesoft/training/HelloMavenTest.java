358d4236cb8e62a103991d6792c278b0f48822d0
Shyam Ramatenki <sramatenki@pacificbiosciences.com>
2018-01-30 10:33:59 -0800

    Added Obj-to-JSON and Testcase#2

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -11,12 +12,21 @@ public class HelloMavenTest extends FunctionalTestCase {
 
 	@Rule
 	public DynamicPort myPort = new DynamicPort("http.port");
+	
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
     	System.out.println("\n\n Testcase-1: Dynamic Http Port --------->" +myPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	System.out.println("\n\n Testcase-2: Dynamic Http Port --------->" +myPort.getNumber() + "\n\n");
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }
+
     @Override
     protected String getConfigFile() {
         return "maven-project.xml";
